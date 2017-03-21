07d45f6fd93d91218febb24ae11e92307bacb6ca
Kaique da Silva <kaique.developer@gmail.com>
2017-03-21 13:04:06 -0300

    Hands spec implemented

---
+++ src/test/scala/example/FeetSpec.scala
@@ -0,0 +1,12 @@
+package example
+
+import org.scalatest._
+
+class FeetSpec extends FlatSpec with Matchers {
+  "The feet class" should "have axis points" in {
+    val feet = new Feet()
+    feet.x shouldEqual 0
+    feet.y shouldEqual 0
+    feet.z shouldEqual 0
+  }
+}
---
+++ src/test/scala/example/HandsSpec.scala
@@ -0,0 +1,10 @@
+package example
+
+import org.scalatest._
+
+class HandsSpec extends FlatSpec with Matchers {
+  "The Hands class" should "have a distribution method" in {
+    val hands = new Hands(10, 40)
+    hands.blcPosSub() shouldEqual 30
+  }
+}
