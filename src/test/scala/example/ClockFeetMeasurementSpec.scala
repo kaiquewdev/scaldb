ffd5adb659dec5570c92a7ef65641a6f30fea06b
Kaique da Silva <kaique.developer@gmail.com>
2017-03-21 13:11:08 -0300

    Specs divided into specific files

---
+++ src/test/scala/example/ClockFeetMeasurementSpec.scala
@@ -0,0 +1,53 @@
+package example
+
+import org.scalatest._
+
+class ClockFeetMeasurementSpec extends FlatSpec with Matchers {
+  "The clock feet measurement class" should "be flexible for clock and feet" in {
+    val clockFeetMeasurement = new ClockFeetMeasurement()
+
+    clockFeetMeasurement.first.x shouldEqual 0
+    clockFeetMeasurement.first.y shouldEqual 0
+    clockFeetMeasurement.first.z shouldEqual 0
+
+    clockFeetMeasurement.second.x shouldEqual 0
+    clockFeetMeasurement.second.y shouldEqual 0
+    clockFeetMeasurement.second.z shouldEqual 0
+  }
+
+  "The clock feet measurement class" should "have a distance between both x axis point" in {
+    val clockFeetMeasurement = new ClockFeetMeasurement()
+
+    clockFeetMeasurement.first.increaseX(10)
+    clockFeetMeasurement.first.x shouldEqual 10
+
+    clockFeetMeasurement.second.increaseX(30)
+    clockFeetMeasurement.second.x shouldEqual 30
+
+    clockFeetMeasurement.distanceX() shouldEqual 20
+  }
+
+  "The clock feet measurement class" should "have a distance between both y axis point" in {
+    val clockFeetMeasurement = new ClockFeetMeasurement()
+
+    clockFeetMeasurement.first.increaseY(10)
+    clockFeetMeasurement.first.y shouldEqual 10
+
+    clockFeetMeasurement.second.increaseY(30)
+    clockFeetMeasurement.second.y shouldEqual 30
+
+    clockFeetMeasurement.distanceY() shouldEqual 20
+  }
+
+  "The clock feet measurement class" should "have a distance between both z axis point" in {
+    val clockFeetMeasurement = new ClockFeetMeasurement()
+
+    clockFeetMeasurement.first.increaseZ(10)
+    clockFeetMeasurement.first.z shouldEqual 10
+
+    clockFeetMeasurement.second.increaseZ(30)
+    clockFeetMeasurement.second.z shouldEqual 30
+
+    clockFeetMeasurement.distanceZ() shouldEqual 20
+  }
+}
