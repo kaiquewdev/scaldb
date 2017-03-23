6968ace0628f05059cb4cf725d31c6e947463a20
Kaique da Silva <kaique.developer@gmail.com>
2017-03-23 11:14:48 -0300

    Refactor on class for clock to feet measurement

--- src/main/scala/shaft_alignment/ClockFeetMeasurement.scala
+++ src/main/scala/shaft_alignment/ClockFeetMeasurement.scala
@@ -1,7 +1,24 @@
 package shaft_alignment
 
-class ClockFeetMeasurement(val first: Clock = (new Clock()), val second: Feet = (new Feet())) extends Object {
-  def distanceX() = (new Hands(first.x,second.x)).blcPosSub()
-  def distanceY() = (new Hands(first.y,second.y)).blcPosSub()
-  def distanceZ() = (new Hands(first.z,second.z)).blcPosSub()
+object InitClock {
+  def instance() = (new Clock())
+}
+
+object InitFeet {
+  def instance() = (new Feet())
+}
+
+object HandsBlcPosSub {
+  def dis(firstAxis: Int, secondAxis: Int) = (new Hands(firstAxis,secondAxis)).blcPosSub()
+}
+
+class ComponentsClockFeetMeasurement(
+  val first: Clock = InitClock.instance,
+  val second: Feet = InitFeet.instance
+) extends Object {}
+
+class ClockFeetMeasurement() extends ComponentsClockFeetMeasurement {
+  def distanceX() = HandsBlcPosSub.dis(first.x,second.x)
+  def distanceY() = HandsBlcPosSub.dis(first.y,second.y)
+  def distanceZ() = HandsBlcPosSub.dis(first.z,second.z)
 }
