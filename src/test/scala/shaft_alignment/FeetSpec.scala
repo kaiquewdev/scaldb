339de1d8c1b6bdd4950805283e5ba0e8d7e1e36a
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-25 14:07:52 -0300

    Test refactor on feet component for measurement

--- src/test/scala/shaft_alignment/FeetSpec.scala
+++ src/test/scala/shaft_alignment/FeetSpec.scala
@@ -5,8 +5,20 @@ import org.scalatest._
 class FeetSpec extends FlatSpec with Matchers {
   "The feet class" should "have axis points" in {
     val feet = new Feet()
+
     feet.x shouldEqual 0
+    feet.getX() shouldEqual 0
+    feet.setX(1000)
+    feet.getX() shouldEqual 1000
+
     feet.y shouldEqual 0
+    feet.getY() shouldEqual 0
+    feet.setY(2000)
+    feet.getY() shouldEqual 2000
+
     feet.z shouldEqual 0
+    feet.getZ() shouldEqual 0
+    feet.setZ(3000)
+    feet.getZ() shouldEqual 3000
   }
 }
