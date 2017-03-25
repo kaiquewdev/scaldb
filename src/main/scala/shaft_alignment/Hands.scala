91d0319ce6b80718d46de87535f9e66e75205ced
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-25 14:15:58 -0300

    Supporting a basic structure for parametrized values inside hands

--- src/main/scala/shaft_alignment/Hands.scala
+++ src/main/scala/shaft_alignment/Hands.scala
@@ -1,6 +1,11 @@
 package shaft_alignment
 
-class Hands(var left: Int = 0, var right: Int = 0) {
+trait HandsAttrs {
+  var left: Int
+  var right: Int
+}
+
+class Hands(var left: Int = 0, var right: Int = 0) extends Object with HandsAttrs {
   def blcPosSub() = Math.max(left, right) - Math.min(left,right)
 }
 
