f2ecd8c6556ee52d2aa8c7713c086e5b4a4e32ed
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-26 23:07:29 -0300

    Supported the clock class

--- src/test/scala/shaft_alignment/ClockSpec.scala
+++ src/test/scala/shaft_alignment/ClockSpec.scala
@@ -5,9 +5,21 @@ import org.scalatest._
 class ClockSpec extends FlatSpec with Matchers {
   "The clock class" should "have default points" in {
     val clock = new Clock()
+
     clock.x shouldEqual 0
+    clock.getX() shouldEqual 0
+    clock.setX(1000)
+    clock.getX() shouldEqual 1000
+
     clock.y shouldEqual 0
+    clock.getY() shouldEqual 0
+    clock.setY(2000)
+    clock.getY() shouldEqual 2000
+
     clock.z shouldEqual 0
+    clock.getZ() shouldEqual 0
+    clock.setZ(3000)
+    clock.getZ() shouldEqual 3000
   }
 
   "The clock class" should "have a degree for a rotation" in {
