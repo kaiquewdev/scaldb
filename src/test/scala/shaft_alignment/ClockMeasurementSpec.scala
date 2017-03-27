bada13192b0bb94073befaab7f97b381ca3dde19
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-27 19:36:31 -0300

    Covering the type setting on the classes

--- src/test/scala/shaft_alignment/ClockMeasurementSpec.scala
+++ src/test/scala/shaft_alignment/ClockMeasurementSpec.scala
@@ -6,39 +6,63 @@ class ClockMeasurementSpec extends FlatSpec with Matchers {
   "The clock measurement class" should "be flexible for clocks" in {
     val clockMeasurement = new ClockMeasurement()
 
+    clockMeasurement.isInstanceOf[ClockMeasurement] shouldEqual true
+
     clockMeasurement.first.x shouldEqual 0
+    clockMeasurement.first.x.isInstanceOf[Int] shouldEqual true
     clockMeasurement.first.getX() shouldEqual 0
+    clockMeasurement.first.getX().isInstanceOf[Int] shouldEqual true
     clockMeasurement.first.setX(1000)
     clockMeasurement.first.getX() shouldEqual 1000
+    clockMeasurement.first.getX().isInstanceOf[Int]
 
     clockMeasurement.first.y shouldEqual 0
+    clockMeasurement.first.y.isInstanceOf[Int] shouldEqual true
     clockMeasurement.first.getY() shouldEqual 0
+    clockMeasurement.first.getY().isInstanceOf[Int] shouldEqual true
     clockMeasurement.first.setY(2000)
     clockMeasurement.first.getY() shouldEqual 2000
+    clockMeasurement.first.getY().isInstanceOf[Int] shouldEqual true
 
     clockMeasurement.first.z shouldEqual 0
+    clockMeasurement.first.z.isInstanceOf[Int] shouldEqual true
     clockMeasurement.first.getZ() shouldEqual 0
+    clockMeasurement.first.getZ().isInstanceOf[Int] shouldEqual true
     clockMeasurement.first.setZ(3000)
     clockMeasurement.first.getZ() shouldEqual 3000
+    clockMeasurement.first.getZ().isInstanceOf[Int] shouldEqual true
 
     clockMeasurement.first.rotation.degree shouldEqual 0
+    clockMeasurement.first.rotation.degree.isInstanceOf[Int] shouldEqual true
 
     clockMeasurement.second.x shouldEqual 0
+    clockMeasurement.second.x.isInstanceOf[Int] shouldEqual true
     clockMeasurement.second.getX() shouldEqual 0
+    clockMeasurement.second.getX().isInstanceOf[Int] shouldEqual true
     clockMeasurement.second.setX(1000)
     clockMeasurement.second.getX() shouldEqual 1000
+    clockMeasurement.second.getX().isInstanceOf[Int] shouldEqual true
 
     clockMeasurement.second.y shouldEqual 0
+    clockMeasurement.second.y.isInstanceOf[Int] shouldEqual true
     clockMeasurement.second.getY() shouldEqual 0
+    clockMeasurement.second.getY().isInstanceOf[Int] shouldEqual true
     clockMeasurement.second.setY(2000)
     clockMeasurement.second.getY() shouldEqual 2000
+    clockMeasurement.second.getY().isInstanceOf[Int] shouldEqual true
 
     clockMeasurement.second.z shouldEqual 0
+    clockMeasurement.second.z.isInstanceOf[Int] shouldEqual true
     clockMeasurement.second.getZ() shouldEqual 0
+    clockMeasurement.second.getZ().isInstanceOf[Int] shouldEqual true
     clockMeasurement.second.setZ(3000)
     clockMeasurement.second.getZ() shouldEqual 3000
+    clockMeasurement.second.getZ().isInstanceOf[Int] shouldEqual true
 
     clockMeasurement.second.rotation.degree shouldEqual 0
+    clockMeasurement.second.rotation.setDeg(10)
+    clockMeasurement.second.rotation.getDeg() shouldEqual 10
+    clockMeasurement.second.rotation.getDeg().isInstanceOf[Int] shouldEqual true
   }
 
   "The clock measurement class" should "have a distance between both x axis point" in {
