fe79edd1e5db3b46e34b1ebaf41e17f8636dd158
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-27 15:10:09 -0300

    Rudimentary aspects of type testing covered

--- src/test/scala/shaft_alignment/RotationSpec.scala
+++ src/test/scala/shaft_alignment/RotationSpec.scala
@@ -5,33 +5,72 @@ import org.scalatest._
 class RotationSpec extends FlatSpec with Matchers {
   "The rotation class" should "have a degree" in {
     val rotation = new Rotation()
+
+    rotation.isInstanceOf[Rotation] shouldEqual true
+
     rotation.degree shouldEqual 0
+    rotation.degree.isInstanceOf[Int] shouldEqual true
+
+    rotation.getDeg() shouldEqual 0
+    rotation.setDeg(75)
+    rotation.getDeg() shouldEqual 75
+    rotation.getDeg().isInstanceOf[Int] shouldEqual true
   }
 
   "The rotation class" should "have getter and setter" in {
     val rotation = new Rotation()
+
+    rotation.isInstanceOf[Rotation] shouldEqual true
+
     rotation.degree shouldEqual 0
-    rotation.setDeg(10)
-    rotation.getDeg() shouldEqual 10
-    rotation.setDeg(20)
-    rotation.getDeg() shouldEqual 20
+    rotation.degree.isInstanceOf[Int] shouldEqual true
+
+    rotation.setDeg(110)
+    rotation.getDeg() shouldEqual 110
+    rotation.getDeg().isInstanceOf[Int] shouldEqual true
+
+    rotation.setDeg(720)
+    rotation.getDeg() shouldEqual 720
+    rotation.getDeg().isInstanceOf[Int] shouldEqual true
   }
 
   "The rotation class" should "have an degree comparison to not equal zero" in {
     val rotation = new Rotation()
+
+    rotation.isInstanceOf[Rotation] shouldEqual true
+
     rotation.degree shouldEqual 0
+    rotation.degree.isInstanceOf[Int] shouldEqual true
+
     rotation.setDeg(10)
     rotation.getDeg() shouldEqual 10
+    rotation.getDeg().isInstanceOf[Int] shouldEqual true
+
     rotation.degNotEqZero() shouldEqual true
+    rotation.degNotEqZero().isInstanceOf[Boolean] shouldEqual true
   }
 
   "The rotation class" should "be increase" in {
     val rotation = new Rotation()
+
+    rotation.isInstanceOf[Rotation] shouldEqual true
+
     rotation.degree shouldEqual 0
+    rotation.degree.isInstanceOf[Int] shouldEqual true
+    rotation.getDeg() shouldEqual 0
+    rotation.getDeg().isInstanceOf[Int] shouldEqual true
+
     rotation.increase(10)
     rotation.degree shouldEqual 10
+    rotation.degree.isInstanceOf[Int] shouldEqual true
+    rotation.getDeg() shouldEqual 10
+    rotation.getDeg().isInstanceOf[Int] shouldEqual true
+
     rotation.increase(20)
     rotation.degree shouldEqual 30
+    rotation.degree.isInstanceOf[Int]
+    rotation.getDeg() shouldEqual 30
+    rotation.getDeg().isInstanceOf[Int]
   }
 
   "The rotation class" should "be decrease" in {
