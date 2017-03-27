c5fad051adf57e7bc680a601e7411e031097a49b
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-26 23:01:09 -0300

    Supporting the clock measurement methods trait

--- src/main/scala/shaft_alignment/ClockMeasurement.scala
+++ src/main/scala/shaft_alignment/ClockMeasurement.scala
@@ -5,13 +5,19 @@ trait Clocks {
   val second: Clock
 }
 
+trait ClockMeasurementMethods {
+  def distanceX(): Int
+  def distanceY(): Int
+  def distanceZ(): Int
+}
+
 class ComponentsClockMeasurement(
   val first: Clock = InitClock.instance,
   val second: Clock = InitClock.instance
 ) extends Object with Clocks {}
 
-class ClockMeasurement() extends ComponentsClockMeasurement {
-  def distanceX() = HandsBlcPosSub.dis(first.x,second.x)
-  def distanceY() = HandsBlcPosSub.dis(first.y,second.y)
-  def distanceZ() = HandsBlcPosSub.dis(first.z,second.z)
+class ClockMeasurement() extends ComponentsClockMeasurement with ClockMeasurementMethods {
+  def distanceX(): Int = HandsBlcPosSub.dis(first.x,second.x)
+  def distanceY(): Int = HandsBlcPosSub.dis(first.y,second.y)
+  def distanceZ(): Int = HandsBlcPosSub.dis(first.z,second.z)
 }
