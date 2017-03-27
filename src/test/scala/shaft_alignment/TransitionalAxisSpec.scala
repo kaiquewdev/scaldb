03b3ddcc0940e9914d63468e13cfb89b10fc486b
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-27 16:28:02 -0300

    Type testing of class and of methods

--- src/test/scala/shaft_alignment/TransitionalAxisSpec.scala
+++ src/test/scala/shaft_alignment/TransitionalAxisSpec.scala
@@ -6,19 +6,30 @@ class TransitionalAxisSpec extends FlatSpec with Matchers {
   "The transitional axis class" should "has incs and decs" in {
     val transitionalAxis = new TransitionalAxis()
 
+    transitionalAxis.isInstanceOf[TransitionalAxis] shouldEqual true
+
     transitionalAxis.incX(10)
     transitionalAxis.getX() shouldEqual 10
+    transitionalAxis.getX().isInstanceOf[Int] shouldEqual true
+
     transitionalAxis.decX(5)
     transitionalAxis.getX() shouldEqual 5
+    transitionalAxis.getX().isInstanceOf[Int] shouldEqual true
 
     transitionalAxis.incY(15)
     transitionalAxis.getY() shouldEqual 15
+    transitionalAxis.getX().isInstanceOf[Int] shouldEqual true
+
     transitionalAxis.decY(5)
     transitionalAxis.getY() shouldEqual 10
+    transitionalAxis.getY().isInstanceOf[Int] shouldEqual true
 
     transitionalAxis.incZ(20)
     transitionalAxis.getZ() shouldEqual 20
+    transitionalAxis.getZ().isInstanceOf[Int] shouldEqual true
+
     transitionalAxis.decZ(5)
     transitionalAxis.getZ() shouldEqual 15
+    transitionalAxis.getZ().isInstanceOf[Int] shouldEqual true
   }
 }
