9635ec811ab6be3ec50f9d12f916991f181ba93c
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-26 23:21:47 -0300

    Supporting the axis class instantiation type test on spec

--- src/test/scala/shaft_alignment/AxisSpec.scala
+++ src/test/scala/shaft_alignment/AxisSpec.scala
@@ -5,6 +5,9 @@ import org.scalatest._
 class AxisSpec extends FlatSpec with Matchers {
   "The axis class" should "has points" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.x shouldEqual 0
     axis.y shouldEqual 0
     axis.z shouldEqual 0
@@ -12,6 +15,8 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "have getters and setters point" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
 
     axis.x shouldEqual 0
     axis.setX(10)
@@ -28,12 +33,18 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "increase the x points" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseX(10)
     axis.x shouldEqual 10
   }
 
   "The axis class" should "decrease the x points" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.decreaseX(5)
     axis.x shouldEqual -5
     axis.increaseX(15)
@@ -43,12 +54,18 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "increase the y points" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseY(10)
     axis.y shouldEqual 10
   }
 
   "The axis class" should "decrease the y points" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseY(10)
     axis.decreaseY(5)
     axis.y shouldEqual 5
@@ -56,12 +73,18 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "increase the z points" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseZ(10)
     axis.z shouldEqual 10
   }
 
   "The axis class" should "decrease the z points" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseZ(10)
     axis.decreaseZ(5)
     axis.z shouldEqual 5
@@ -69,24 +92,36 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "divide x by a default value" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseX(10)
     axis.divX() shouldEqual 5
   }
 
   "The axis class" should "divide y by a default value" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseY(10)
     axis.divY() shouldEqual 5
   }
 
   "The axis class" should "divide z by a default value" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseZ(10)
     axis.divZ() shouldEqual 5
   }
 
   "The axis class" should "show an history of ins for x" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseX(10)
     axis.increaseHistoryX() shouldEqual List(0)
     axis.increaseX(20)
@@ -97,6 +132,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show an history observable of ins for x" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseX(10)
     axis.increaseX(20)
     axis.increaseX(30)
@@ -105,6 +143,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show an history observable of ins for y" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseY(10)
     axis.increaseY(20)
     axis.increaseY(30)
@@ -113,6 +154,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show an history observable of ins for z" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseZ(10)
     axis.increaseZ(20)
     axis.increaseZ(30)
@@ -121,6 +165,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show an history of ins for y" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseY(10)
     axis.increaseHistoryY() shouldEqual List(0)
     axis.increaseY(20)
@@ -131,6 +178,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show an history of ins outs for z" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseZ(10)
     axis.increaseHistoryZ() shouldEqual List(0)
     axis.increaseZ(20)
@@ -141,6 +191,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show an history of outs for x" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseX(10)
     axis.decreaseX(5)
     axis.decreaseHistoryX() shouldEqual List(0,5)
@@ -151,6 +204,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show a history of observable outs for x" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseX(10)
     axis.decreaseX(5)
     axis.observableHistoryOfDecreasedX().map(v => v*2).toBlocking.toList shouldEqual List(0,10)
@@ -161,6 +217,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show an history of outs for y" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseY(10)
     axis.decreaseY(5)
     axis.decreaseHistoryY() shouldEqual List(0,5)
@@ -171,6 +230,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show a history of observable outs for y" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseY(10)
     axis.decreaseY(5)
     axis.observableHistoryOfDecreasedY().map(v => v*2).toBlocking.toList shouldEqual List(0,10)
@@ -181,6 +243,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show an history of outs for z" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseZ(10)
     axis.decreaseZ(5)
     axis.decreaseHistoryZ() shouldEqual List(0,5)
@@ -191,6 +256,9 @@ class AxisSpec extends FlatSpec with Matchers {
 
   "The axis class" should "show a history of observable outs for z" in {
     val axis = new Axis()
+
+    axis.isInstanceOf[Axis] shouldEqual true
+
     axis.increaseZ(10)
     axis.decreaseZ(5)
     axis.observableHistoryOfDecreasedZ().map(v => v*2).toBlocking.toList shouldEqual List(0,10)
