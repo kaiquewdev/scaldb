76479a51c4907556456f2352f51fdfd2d8a38dc3
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-26 23:09:05 -0300

    Covering the trait for clock

--- src/main/scala/shaft_alignment/Clock.scala
+++ src/main/scala/shaft_alignment/Clock.scala
@@ -1,3 +1,7 @@
 package shaft_alignment
 
-class Clock(var rotation: Rotation = new Rotation()) extends Axis {}
+trait ClockAttr {
+  var rotation: Rotation
+}
+
+class Clock(var rotation: Rotation = new Rotation()) extends Axis with ClockAttr {}
