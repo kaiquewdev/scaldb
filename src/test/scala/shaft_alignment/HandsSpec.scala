7c434c710e33fcbe44c9346f6cbc11a3f7a882dc
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-28 19:56:54 -0300

    Hands class type coverage

--- src/test/scala/shaft_alignment/HandsSpec.scala
+++ src/test/scala/shaft_alignment/HandsSpec.scala
@@ -5,6 +5,10 @@ import org.scalatest._
 class HandsSpec extends FlatSpec with Matchers {
   "The Hands class" should "have a distribution method" in {
     val hands = new Hands(10, 40)
+
+    hands.isInstanceOf[Hands] shouldEqual true
+
     hands.blcPosSub() shouldEqual 30
+    hands.blcPosSub().isInstanceOf[Int] shouldEqual true
   }
 }
