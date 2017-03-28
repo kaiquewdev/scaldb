656f04720c1f568de745f92695863a7aadb9be89
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-28 19:52:41 -0300

    Supporting the feet class

--- src/test/scala/shaft_alignment/FeetSpec.scala
+++ src/test/scala/shaft_alignment/FeetSpec.scala
@@ -6,19 +6,30 @@ class FeetSpec extends FlatSpec with Matchers {
   "The feet class" should "have axis points" in {
     val feet = new Feet()
 
+    feet.isInstanceOf[Feet] shouldEqual true
+
     feet.x shouldEqual 0
+    feet.x.isInstanceOf[Int] shouldEqual true
     feet.getX() shouldEqual 0
+    feet.getX().isInstanceOf[Int] shouldEqual true
     feet.setX(1000)
     feet.getX() shouldEqual 1000
+    feet.getX().isInstanceOf[Int] shouldEqual true
 
     feet.y shouldEqual 0
+    feet.y.isInstanceOf[Int] shouldEqual true
     feet.getY() shouldEqual 0
+    feet.getY().isInstanceOf[Int] shouldEqual true
     feet.setY(2000)
     feet.getY() shouldEqual 2000
+    feet.getY().isInstanceOf[Int] shouldEqual true
 
     feet.z shouldEqual 0
+    feet.z.isInstanceOf[Int] shouldEqual true
     feet.getZ() shouldEqual 0
+    feet.getZ().isInstanceOf[Int] shouldEqual true
     feet.setZ(3000)
     feet.getZ() shouldEqual 3000
+    feet.getZ().isInstanceOf[Int] shouldEqual true
   }
 }
