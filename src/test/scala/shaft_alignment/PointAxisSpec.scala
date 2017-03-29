1dc30a06a3d2d12b6ec619bcb6914cbcc78caa0e
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-29 07:52:43 -0300

    Testing and covering point axis types

--- src/test/scala/shaft_alignment/PointAxisSpec.scala
+++ src/test/scala/shaft_alignment/PointAxisSpec.scala
@@ -5,8 +5,14 @@ import org.scalatest._
 class PointAxisSpec extends FlatSpec with Matchers {
   "The point axis class" should "has points" in {
     val pointAxis = new PointAxis()
+
+    pointAxis.isInstanceOf[PointAxis] shouldEqual true
+
     pointAxis.x shouldEqual 0
+    pointAxis.x.isInstanceOf[Int] shouldEqual true
     pointAxis.y shouldEqual 0
+    pointAxis.y.isInstanceOf[Int] shouldEqual true
     pointAxis.z shouldEqual 0
+    pointAxis.z.isInstanceOf[Int] shouldEqual true
   }
 }
