9f4f549db644150d43b8179e27b6abd2e7be153c
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-29 07:46:34 -0300

    Tested the type of initial point value

--- src/test/scala/shaft_alignment/InitialPointSpec.scala
+++ src/test/scala/shaft_alignment/InitialPointSpec.scala
@@ -5,5 +5,6 @@ import org.scalatest._
 class InitialPointSpec extends FlatSpec with Matchers {
   "The initial point object" should "has a default value" in {
     InitialPoint.value() shouldEqual (new Point()).value
+    InitialPoint.value().isInstanceOf[Int] shouldEqual true
   }
 }
