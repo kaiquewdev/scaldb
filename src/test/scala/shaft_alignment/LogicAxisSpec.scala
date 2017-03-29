5847c4490a9a2630f0a1a712c3995e0b0006c127
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-29 07:48:58 -0300

    Testing the logic axis methods

--- src/test/scala/shaft_alignment/LogicAxisSpec.scala
+++ src/test/scala/shaft_alignment/LogicAxisSpec.scala
@@ -6,13 +6,18 @@ class LogicAxisSpec extends FlatSpec with Matchers {
   "The logic axis class" should "has getters and setters" in {
     val logicAxis = new LogicAxis()
 
+    logicAxis.isInstanceOf[LogicAxis] shouldEqual true
+
     logicAxis.setX(10)
     logicAxis.getX() shouldEqual 10
+    logicAxis.getX().isInstanceOf[Int] shouldEqual true
 
     logicAxis.setY(15)
     logicAxis.getY() shouldEqual 15
+    logicAxis.getY().isInstanceOf[Int] shouldEqual true
 
     logicAxis.setZ(20)
     logicAxis.getZ() shouldEqual 20
+    logicAxis.getZ().isInstanceOf[Int] shouldEqual true
   }
 }
