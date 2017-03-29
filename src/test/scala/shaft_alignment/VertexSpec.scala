a626db4eed9e9762e9f49debb76b2b3759e4d7e7
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-29 13:06:47 -0300

    Covering the other clusterized tests for raw and nested tests

--- src/test/scala/shaft_alignment/VertexSpec.scala
+++ src/test/scala/shaft_alignment/VertexSpec.scala
@@ -41,20 +41,55 @@ class VertexSpec extends FlatSpec with Matchers {
     vertex.point.value.isInstanceOf[Int] shouldEqual true
   }
 
-  "The vertex class" should "have a small instance" in {
+  "The vertex class" should "have a raw method to set a value of the point" in {
+    val vertex = new Vertex()
+
+    vertex.isInstanceOf[Vertex] shouldEqual true
+
+    vertex.setPointValue(20) shouldEqual 20
+   }
+
+   "The vertex class" should "have a raw method to get a value of the point" in {
     val vertex = new Vertex()
 
     vertex.isInstanceOf[Vertex] shouldEqual true
 
     vertex.setPointValue(20) shouldEqual 20
     vertex.getPointValue() shouldEqual 20
-    vertex.getPointValue().isInstanceOf[Int] shouldEqual true
+   }
 
-    vertex.setPointValue(10).isInstanceOf[Int] shouldEqual true
+   "The vertex class" should "have a raw method to verify the type of getter" in {
+    val vertex = new Vertex()
+
+    vertex.isInstanceOf[Vertex] shouldEqual true
+
+    vertex.getPointValue().isInstanceOf[Int] shouldEqual true
+   }
+
+   "The vertex class" should "have a raw method to verify the type of setter" in {
+     val vertex = new Vertex()
+
+     vertex.isInstanceOf[Vertex] shouldEqual true
+
+     vertex.setPointValue(10).isInstanceOf[Int] shouldEqual true
+   }
+
+   "The vertex class" should "have a nested method to set a value" in {
+    val vertex = new Vertex()
+
+    vertex.isInstanceOf[Vertex] shouldEqual true
 
     vertex.point.setValue(30) shouldEqual 30
+   }
+
+   "The vertex class" should "have a nested method type boolean comparison" in {
+    val vertex = new Vertex()
+
+    vertex.isInstanceOf[Vertex] shouldEqual true
+
     vertex.point.setValue(30).isInstanceOf[Int] shouldEqual true
-    vertex.point.getValue() shouldEqual 30
-    vertex.point.getValue().isInstanceOf[Int] shouldEqual true
-  }
+   }
+
+    //vertex.point.getValue() shouldEqual 30
+    //vertex.point.getValue().isInstanceOf[Int] shouldEqual true
 }
