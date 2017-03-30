53eaa42a51576d119a8252833eec462e1ae87a9b
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-03-30 14:16:20 -0300

    Supporting the methods of point class on at your own trait

--- src/main/scala/shaft_alignment/Point.scala
+++ src/main/scala/shaft_alignment/Point.scala
@@ -2,12 +2,14 @@ package shaft_alignment
 
 trait PointAttr {
   var value: Int
+  def setValue(n: Int): Int
+  def getValue(): Int
 }
 
 class Point(var value: Int = 0) extends Object with PointAttr {
-  def setValue(n: Int) = {
+  def setValue(n: Int): Int = {
     value = n
     value
   }
-  def getValue() = value
+  def getValue(): Int = value
 }
