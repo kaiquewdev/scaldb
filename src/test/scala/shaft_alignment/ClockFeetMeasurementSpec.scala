78de96cead0e0c165f361ff2505208bb54291b4f
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-04-02 17:43:32 -0300

    Basic coverage of type testing

--- src/test/scala/shaft_alignment/ClockFeetMeasurementSpec.scala
+++ src/test/scala/shaft_alignment/ClockFeetMeasurementSpec.scala
@@ -31,6 +31,21 @@ class ClockFeetMeasurementSpec extends FlatSpec with Matchers {
     clockFeetMeasurement.first.getX() shouldEqual 15
   }
 
+  "The clock feet measurement class" should "have a distance between both x axis point" in {
+    val clockFeetMeasurement = new ClockFeetMeasurement()
+
+    clockFeetMeasurement.first.increaseX(10) shouldEqual 10
+    clockFeetMeasurement.first.x shouldEqual 10
+    clockFeetMeasurement.first.getX() shouldEqual 10
+
+    clockFeetMeasurement.second.increaseX(30) shouldEqual 30
+    clockFeetMeasurement.second.x shouldEqual 30
+    clockFeetMeasurement.second.getX() shouldEqual 30
+
+    clockFeetMeasurement.distanceX() shouldEqual 20
+    clockFeetMeasurement.distanceX().isInstanceOf[Int] shouldEqual true
+  }
+
   "The clock feet measurement class" should "have y axis point for clock" in {
     val clockFeetMeasurement = new ClockFeetMeasurement()
 
@@ -58,55 +73,23 @@ class ClockFeetMeasurementSpec extends FlatSpec with Matchers {
     clockFeetMeasurement.first.z.isInstanceOf[Int] shouldEqual true
   }
 
-  "The clock feet measurement class" should "be flexible for clock and feet" in {
+  "The clock feet measurement class" should "have a getter for z axis point for clock" in {
     val clockFeetMeasurement = new ClockFeetMeasurement()
 
     clockFeetMeasurement.isInstanceOf[ClockFeetMeasurement] shouldEqual true
 
-    clockFeetMeasurement.first.x shouldEqual 0
-    clockFeetMeasurement.first.x.isInstanceOf[Int] shouldEqual true
     clockFeetMeasurement.first.getX() shouldEqual 0
-    clockFeetMeasurement.first.setX(1000)
-    clockFeetMeasurement.first.getX() shouldEqual 1000
-
-    clockFeetMeasurement.first.y shouldEqual 0
-    clockFeetMeasurement.first.getY() shouldEqual 0
-    clockFeetMeasurement.first.setY(2000)
-    clockFeetMeasurement.first.getY() shouldEqual 2000
-
-    clockFeetMeasurement.first.z shouldEqual 0
-    clockFeetMeasurement.first.getZ() shouldEqual 0
-    clockFeetMeasurement.first.setZ(3000)
-    clockFeetMeasurement.first.getZ() shouldEqual 3000
-
-    clockFeetMeasurement.second.x shouldEqual 0
-    clockFeetMeasurement.second.getX() shouldEqual 0
-    clockFeetMeasurement.second.setX(1000)
-    clockFeetMeasurement.second.getX() shouldEqual 1000
-
-    clockFeetMeasurement.second.y shouldEqual 0
-    clockFeetMeasurement.second.getY() shouldEqual 0
-    clockFeetMeasurement.second.setY(2000)
-    clockFeetMeasurement.second.getY() shouldEqual 2000
-
-    clockFeetMeasurement.second.z shouldEqual 0
-    clockFeetMeasurement.second.getZ() shouldEqual 0
-    clockFeetMeasurement.second.setZ(3000)
-    clockFeetMeasurement.second.getZ() shouldEqual 3000
+    clockFeetMeasurement.first.getX().isInstanceOf[Int] shouldEqual true
   }
 
-  "The clock feet measurement class" should "have a distance between both x axis point" in {
+  "The clock feet measurement class" should "have a setter for z axis point for clock" in {
     val clockFeetMeasurement = new ClockFeetMeasurement()
 
-    clockFeetMeasurement.first.increaseX(10)
-    clockFeetMeasurement.first.x shouldEqual 10
-    clockFeetMeasurement.first.getX() shouldEqual 10
+    clockFeetMeasurement.isInstanceOf[ClockFeetMeasurement] shouldEqual true
 
-    clockFeetMeasurement.second.increaseX(30)
-    clockFeetMeasurement.second.x shouldEqual 30
-    clockFeetMeasurement.second.getX() shouldEqual 30
-
-    clockFeetMeasurement.distanceX() shouldEqual 20
+    clockFeetMeasurement.first.setX(10) shouldEqual 10
+    clockFeetMeasurement.first.setX(15).isInstanceOf[Int] shouldEqual true
+    clockFeetMeasurement.first.getX() shouldEqual 15
   }
 
   "The clock feet measurement class" should "have a distance between both y axis point" in {
